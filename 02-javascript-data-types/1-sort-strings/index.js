e96c3742a69ba21be52f36ca437c62e703771f60
antzhlv <zhulev.a.m@gmail.com>
2022-01-17 20:22:15 +0300

    * fix sortString * fix code style

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -6,11 +6,12 @@
  */
 export function sortStrings(arr, param = 'asc') {
 
-  const arrSort = [...arr].sort((a, b) => a.localeCompare(b, ['ru', 'en'], {caseFirst: 'upper'}));
+  const arrSortAsc = [...arr].sort((a, b) => a.localeCompare(b, ['ru', 'en'], {caseFirst: 'upper'}));
+  const arrSortDesc = [...arr].sort((b, a) => b.localeCompare(a, ['ru', 'en'], {caseFirst: 'upper'}));
 
   if (param === 'asc') {
-    return arrSort;
+    return arrSortAsc;
   } else if (param === 'desc') {
-    return arrSort.reverse();
+    return arrSortDesc;
   }
 }
--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -5,5 +5,7 @@
  * @returns {object} - returns the new object
  */
 export const omit = (obj, ...fields) => {
-  return Object.fromEntries(Object.entries(obj).filter(([objectKey]) => !fields.includes(objectKey)));
+  return Object.fromEntries(
+    Object.entries(obj).filter(([objectKey]) => !fields.includes(objectKey))
+  );
 };
