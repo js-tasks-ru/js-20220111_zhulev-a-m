734454e2ca4cd082f5cc27ad722ff5bac1b889b9
antzhlv <zhulev.a.m@gmail.com>
2022-02-03 17:10:47 +0300

    lesson 5

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,64 @@
 export default class NotificationMessage {
+  static active;
+  element;
+  constructor(message = '', {
+    duration = 0,
+    type = '',
+  } = {}) {
+    this.message = message;
+    this.duration = duration;
+    this.type = type;
+    this.render();
+  }
+
+  getTemplate() {
+    return `
+      <div class="notification ${this.type}" style="--value:${(this.duration / 1000) + 's'}">
+        <div class="timer"></div>
+        <div class="inner-wrapper">
+            <div class="notification-header">${this.type}</div>
+            <div class="notification-body">
+                ${this.message}
+            </div>
+        </div>
+      </div>
+    `;
+  }
+
+  render() {
+    const element = document.createElement('div');
+    element.innerHTML = this.getTemplate();
+    this.element = element.firstElementChild;
+  }
+
+  show(element) {
+    if (NotificationMessage.active) {
+      NotificationMessage.active.remove();
+      NotificationMessage.activeNotification = null;
+    }
+
+    NotificationMessage.active = this.element;
+
+    if (element) {
+      element.append(this.element);
+    } else {
+      document.body.append(this.element);
+    }
+
+    setTimeout(() => {
+      this.remove();
+    }, this.duration);
+  }
+
+  remove() {
+    if (this.element) {
+      this.element.remove();
+    }
+  }
+
+  destroy() {
+    this.remove();
+    this.element = null;
+  }
 
 }
--- 05-dom-document-loading/2-sortable-table-v1/index.js
+++ 05-dom-document-loading/2-sortable-table-v1/index.js
@@ -1,6 +1,110 @@
 export default class SortableTable {
-  constructor(headerConfig = [], data = []) {
 
+  subElements = {}
+
+  constructor(headerConfig = [], data = []) {
+    this.headerConfig = headerConfig;
+    this.data = data;
+    this.render();
+  }
+
+  getHeader() {
+    return this.headerConfig.map(el => {
+      return `
+          <div class="sortable-table__cell" data-id=${el.id} data-sortable="${el.sortable}">
+            <span>${el.title}</span>
+            <span data-element="arrow" class="sortable-table__sort-arrow">
+              <span class="sort-arrow"></span>
+            </span>
+          </div>`;
+    }).join('\n');
+  }
+
+  getRows(data) {
+    return data.map((rowData) => {
+      return `<a href="" class="sortable-table__row">${this.getCells(rowData)}</a>`;
+    }).join('');
+  }
+
+  getCells(rowData) {
+    return this.headerConfig.map(config => {
+      const value = rowData[config.id];
+      return config.template ? config.template(value) : `<div class="sortable-table__cell">${value}</div>`;
+    }).join('\n');
+  }
+
+  render() {
+    const element = document.createElement('div');
+    element.innerHTML = `
+    <div class="sortable-table">
+        <div data-element="header" class="sortable-table__header sortable-table__row">
+          ${this.getHeader()}
+        </div>
+        <div data-element="body" class="sortable-table__body">
+          ${this.getRows(this.data)}
+        </div>
+    </div>
+    `;
+    this.element = element.firstElementChild;
+    this.subElements = this.getSubElements(element);
+  }
+
+  getSubElements(element) {
+    const result = {};
+    const elements = element.querySelectorAll('[data-element]');
+
+    for (const subElement of elements) {
+      const name = subElement.dataset.element;
+      result[name] = subElement;
+    }
+
+    return result;
+  }
+
+  sort(field, order) {
+    const sortHeader = this.headerConfig.find(item => item.id === field);
+    if (!sortHeader.sortable) {
+      return;
+    }
+
+    const direction = { asc: 1, desc: -1 }[order];
+    let sortedData;
+    switch (sortHeader.sortType) {
+      case 'string':
+        sortedData = this.sortStrings(this.data, field, direction);
+        break;
+      case 'number':
+        sortedData = this.sortNumbers(this.data, field, direction);
+        break;
+    }
+
+    this.updateSortInfo(field, order);
+    this.subElements.body.innerHTML = this.getRows(sortedData);
+  }
+
+  sortStrings(data, field, direction) {
+    return [...data].sort((a, b) => direction * a[field].localeCompare(b[field], ['ru', 'en']));
+  }
+
+  sortNumbers(data, field, direction) {
+    return [...data].sort((a, b) => direction * (a[field] - b[field]));
+  }
+
+  updateSortInfo(field, order) {
+    this.element
+      .querySelectorAll('[data-id]')
+      .forEach(it => it.getAttribute('data-id') === field ? it.dataset.order = order : '');
+  }
+
+  remove() {
+    if (this.element) {
+      this.element.remove();
+    }
+  }
+
+  destroy() {
+    this.remove();
+    this.element = null;
+    this.subElements = {};
   }
 }
-
